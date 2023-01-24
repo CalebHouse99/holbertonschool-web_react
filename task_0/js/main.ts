c1824e028cd55e2cf4141924f7c13cd3c7f2d320
Caleb House <calebhouse99@gmail.com>
2023-01-24 10:42:32 -0800

    task 0

---
+++ task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = { firstName: 'Phillip', lastName: 'Phillips', age: 32, location: 'Georgia' };
+const student2: Student = { firstName: 'Thomas', lastName: 'Thomas', age: 99, location: 'Wyoming' };
+const studentsList: Array<Student> = [student1, student2];
+
+function CreateTable(table: HTMLTableElement, studentsList: Array<Student>) {
+for (const student of studentsList) {
+  let row: HTMLTableRowElement = table.insertRow();
+  const firstName: HTMLTableCellElement = row.insertCell();
+  const location: HTMLTableCellElement = row.insertCell();
+  firstName.innerHTML = student.firstName;
+  location.innerHTML = student.location;
+}
+document.body.appendChild(table);
+}
+let table: HTMLTableElement = document.createElement('table');
+CreateTable(table, studentsList);
