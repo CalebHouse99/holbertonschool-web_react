caba2930d42226825915b35845513915884ab697
Caleb House <calebhouse99@gmail.com>
2023-03-07 09:00:33 -0800

    updated ts to github

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -6,18 +6,18 @@ interface Student {
 }
 
 const student1: Student = { firstName: 'Phillip', lastName: 'Phillips', age: 32, location: 'Georgia' };
-const student2: Student = { firstName: 'Thomas', lastName: 'Thomas', age: 99, location: 'Wyoming' };
+const student2: Student = { firstName: 'Thomas', lastName: 'Thomason', age: 99, location: 'Wyoming' };
 const studentsList: Array<Student> = [student1, student2];
 
 function CreateTable(table: HTMLTableElement, studentsList: Array<Student>) {
-for (const student of studentsList) {
-  let row: HTMLTableRowElement = table.insertRow();
-  const firstName: HTMLTableCellElement = row.insertCell();
-  const location: HTMLTableCellElement = row.insertCell();
-  firstName.innerHTML = student.firstName;
-  location.innerHTML = student.location;
-}
-document.body.appendChild(table);
+  for (const student of studentsList) {
+    let row: HTMLTableRowElement = table.insertRow();
+    const firstName: HTMLTableCellElement = row.insertCell();
+    const location: HTMLTableCellElement = row.insertCell();
+    firstName.innerHTML = student.firstName;
+    location.innerHTML = student.location;
+  }
+  document.body.appendChild(table);
 }
 let table: HTMLTableElement = document.createElement('table');
 CreateTable(table, studentsList);
