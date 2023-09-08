05e6dbee69bca36cb7f7b18b36539e2615557cc2
unknown <calebhouse99@gmail.com>
2023-09-07 23:20:21 -0500

    tasks 1-3

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        header: './header/header.js',
+        body: './body/body.js',
+        footer: './footer/footer.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[hash][ext][query]'
+                }
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(),
+    ],
+    devServer: {
+        port: 8564,
+        open: true
+    },
+    devtool: 'inline-source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+};
