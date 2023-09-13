b6687ca0a2cd6e29df48c7b2bf93af971a89c517
unknown <calebhouse99@gmail.com>
2023-09-13 12:09:33 -0500

    thru half of task 6. plenty of tests!

---
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,20 @@
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('<App />', () => {
+    test('renders without crashing', () => {
+        shallow(<App />);
+    });
+    test('renders a header with the class App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('header.App-header')).toHaveLength(1);
+    })
+    test('renders a div with the class App-body', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div.App-body')).toHaveLength(1);
+    })
+    test('renders a footer with the class App-footer', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('footer.App-footer')).toHaveLength(1);
+    })
+});
